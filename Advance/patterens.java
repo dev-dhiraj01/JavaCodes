d65520a77643ae3db527a8fac59a547e9da77307
Dhiraj70C <dchaudhari1256@gmail.com>
2026-06-15 16:58:03 +0530

    added code

---
+++ Advance/patterens.java
@@ -0,0 +1,50 @@
+package Advance;
+
+import java.util.Scanner;
+
+public class patterens {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of n: ");
+        // int n = sc.nextInt();
+        int n=5;
+        System.out.println("size of n is :"+ n);
+
+        for(int i = 0 ; i < n;i++){
+            for(int j = 0 ; j <= i ; j++){
+                System.out.print(" *");
+            }
+            System.out.println("");
+        }
+
+        System.out.println("print second pattern : ");
+        for(int i = n ; i > 0;i--){
+            for(int j = 0 ; j < i ; j++){
+                System.out.print(" *");
+            }
+            System.out.println("");
+        }
+
+        System.out.println("print third pattern : ");
+        for(int i = 0 ; i < n;i++){
+            for(int k = 0;k < n-i;k++){
+                System.out.println(" ");
+            }
+            for(int j = 0 ; j < i ; j++){
+                System.out.print(" *");
+            }
+            System.out.println("");
+        }
+        sc.close();
+        int arr[] ={1,2,3,4,5};
+        int arr1[] = arr; 
+         
+        for(int i = 0 ; i < arr1.length;i++){
+            arr1[i] = arr1[i]*2;
+        }
+        
+        for(int i = 0 ; i < arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
---
+++ Advance/student.java
@@ -0,0 +1,39 @@
+package Advance;
+
+import java.util.Scanner;
+
+public class student {
+    public String name;
+    public int age;
+    public String Cname;
+    private int pocketMoney;
+
+     student(int pocketMoney) {
+        this.pocketMoney = pocketMoney;
+    }
+    void setdata(String name, int age, String Cname) {
+        this.name = name;
+        this.age = age;
+        this.Cname = Cname;
+    }
+    void getdata(){
+        System.out.println("Name: "+name);
+        System.out.println("age: "+age);
+        System.out.println("College name : "+Cname);
+        System.out.println("Pocket Money : "+pocketMoney);
+    }
+    public static void main(String[] args) {
+        student st = new student(1000);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the name of student : ");
+        st.name = sc.nextLine();
+        System.out.println("Enter the age of student : ");
+        st.age = sc.nextInt();
+        System.out.println("Enter the college name of student : ");
+        st.Cname = sc.next();
+        
+        st.getdata();
+        sc.close();
+    }
+}
