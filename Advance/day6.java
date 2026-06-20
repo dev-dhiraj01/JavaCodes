8101357bd0a5d7a0d472475e760ca37afc0f034a
Dhiraj70C <dchaudhari1256@gmail.com>
2026-06-20 13:55:49 +0530

    added the mission code

--- Advance/day6.java
+++ Advance/day6.java
@@ -5,8 +5,19 @@ import java.util.Scanner;
 //the payload the data object reference occupying that specific memory 
 // the mechanism instead of a scequential linear search which results an o(n) time complexity the sys applies a deterministic mechanism to resolve the exact index instently by bypassing iterative traversal the data payload is retrive in o(1) constant time 
 
+//int the yrs spy uni agents like pathan tiger and kabir must complete a high risk mission vefore a countdown reatches 0 the mission control sys displays the remaining countdown at every second the intelligence team wants to implement this countdown using recurssion instead of loops input is coutndowm 5 output mission starts in 5 
 class day6{
 
+static void countdown(int n){
+
+    if(n >= 1){
+        System.out.println( n);
+        countdown(n-1);
+    }else{
+        System.out.println("mission Activated ");
+    }
+
+}
 static void access(String id, int s, int e) {
     if (s >= e) {
         System.out.println("Access Granted...");
@@ -42,7 +53,10 @@ static void access(String id, int s, int e) {
         int level = sc.nextInt();
         int total = quantumjumps(level);
         System.out.println("the total no of jumps are : " + total);
-
+        
+        int time = 5;
+        System.out.println("mission Starts in ");
+        countdown(time);
         sc.close();
     }
 }
