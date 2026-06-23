7b17122c4c2129eeb8f89e971ba17dbd7676afb6
Dhiraj70C <dchaudhari1256@gmail.com>
2026-06-23 12:53:59 +0530

    added bit manipulation code

---
+++ Advance/Day8.java
@@ -0,0 +1,13 @@
+class Day8{
+
+    public static void main(String arge[]){
+
+        int cap = 5;
+        for(int i = 0; i < 2;i++){
+            cap = cap<<1;
+        }
+        System.out.println(cap);
+    }
+}
+
+//capacity = 5 years = 2
