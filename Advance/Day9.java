4be817d10872f001aba66721e323096ce60cc8c5
Dhiraj70C <dchaudhari1256@gmail.com>
2026-06-24 14:43:41 +0530

    added code

--- Advance/Day9.java
+++ Advance/Day9.java
@@ -1,33 +1,137 @@
-class Day9{
+import java.util.*;
 
-    static int binSearch(int arr[], int s , int e, int target){
+public class Day9 {
 
-        while(s<=e){
-            int mid = s + (e-s)/2;
+    // Task 5: Binary Search Method
+    public static int binarySearch(int[] arr, int target) {
+        int s = 0, e = arr.length - 1;
 
-            if(arr[mid] == target){
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+
+            if (arr[mid] == target) {
                 return mid;
-            }
-            if(arr[mid] < target){
-                s = mid+1;
-            }
-            else{
-                e = mid -1;
+            } else if (arr[mid] < target) {
+                s = mid + 1;
+            } else {
+                e = mid - 1;
             }
         }
-            return -1;
+        return -1;
     }
-    public static void main(String args[]){
-        int Box[] = {12,23,34,45,67,89};
-        int target = 67;
 
-        for(int i = 0; i < Box.length; i++){
+    // Task 6: Recursion Method
+    public static int totalPoints(int days) {
+        if (days == 1) {
+            return 1;
+        }
+        return days + totalPoints(days - 1);
+    }
 
-            if(Box[i] == target){
-                System.out.println("laddu box found at index : "+i);
+    // Task 4: Prime Check Method
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        int[] chocolates = {5, 8, 3, 10, 6};
+        String secretMessage = "LEVEL";
+        String couponCode = "aabbcde";
+        int[] prizes = {10, 20, 30, 40, 50, 60};
+        int luckyToken = 29;
+        int permissions = 5;      // 0101
+        int bonusPermission = 3;  // 0011
+        int days = 5;
+        int target = 40;
+
+        // Task 1
+
+        int totalChocolates = 0;
+        int maxChocolates = chocolates[0];
+
+        for (int choco : chocolates) {
+            totalChocolates += choco;
+            if (choco > maxChocolates) {
+                maxChocolates = choco;
             }
         }
 
-        System.out.println(binSearch(Box,0,Box.length,67));
+        System.out.println("Total Chocolates = " + totalChocolates);
+        System.out.println("Maximum Chocolates = " + maxChocolates);
+        System.out.print("Reverse = ");
+        for (int i = chocolates.length - 1; i >= 0; i--) {
+            System.out.print(chocolates[i] + " ");
+        }
+        System.out.println();
+
+        // Task 2
+        String reversed = new StringBuilder(secretMessage).reverse().toString();
+
+        if (secretMessage.equals(reversed)) {
+            System.out.println("Secret Message Verified");
+        } else {
+            System.out.println("Secret Message Not Verified");
+        }
+
+
+        // Task 3
+        HashMap<Character, Integer> map = new LinkedHashMap<>();
+
+        for (char ch : couponCode.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+        }
+
+        char firstUnique = '\0';
+        for (char ch : couponCode.toCharArray()) {
+            if (map.get(ch) == 1) {
+                firstUnique = ch;
+                break;
+            }
+        }
+
+        System.out.println("First Unique Character = " + firstUnique);
+
+
+        // Task 4
+
+        if (isPrime(luckyToken)) {
+            System.out.println("Lucky Token");
+        } else {
+            System.out.println("Not a Lucky Token");
+        }
+
+        // Task 5
+
+        int index = binarySearch(prizes, target);
+
+        if (index != -1) {
+            System.out.println("Prize Found at Index " + index);
+        } else {
+            System.out.println("Prize Not Found");
+        }
+
+        // Task 6
+
+        System.out.println(totalPoints(days));
+
+        // Task 7
+
+        System.out.println("AND = " + (permissions & bonusPermission));
+        System.out.println("OR = " + (permissions | bonusPermission));
+        System.out.println("XOR = " + (permissions ^ bonusPermission));
+        System.out.println("Left Shift = " + (permissions << 1));
+        System.out.println("Right Shift = " + (permissions >> 1));
+        
     }
 }
+// doremon maintains a digital inventory of gajets inside its 4d pockate to quickly locate a gaget the inventory sys automatically arrenge all gaget ids in ascending order one day nobita argently needs a gaget to complete his homework since the inventory contains thousants of gagets checking each gaget one by one would take toomuch time doremon decides an efficient search technique that repeatedly narrows the search space by half until the require gaget is found gagdet 105 105 110 115 120 125 139 t = 120
