48e6547e071ea6202463ab9cfc3fdd6d4d074f7b
Dhiraj70C <dchaudhari1256@gmail.com>
2026-06-24 11:56:38 +0530

    added code for binary search

--- Advance/Day9.java
+++ Advance/Day9.java
@@ -1,14 +1,33 @@
 class Day9{
 
-    public static void main(String args[]){
-        int arr[] = {1,2,3,4,5};
-        int target = 5;
+    static int binSearch(int arr[], int s , int e, int target){
 
-        for(int i = 0; i < 5; i++){
+        while(s<=e){
+            int mid = s + (e-s)/2;
 
-            if(arr[i] == target){
-                System.out.println(i);
+            if(arr[mid] == target){
+                return mid;
+            }
+            if(arr[mid] < target){
+                s = mid+1;
+            }
+            else{
+                e = mid -1;
             }
         }
+            return -1;
+    }
+    public static void main(String args[]){
+        int Box[] = {12,23,34,45,67,89};
+        int target = 67;
+
+        for(int i = 0; i < Box.length; i++){
+
+            if(Box[i] == target){
+                System.out.println("laddu box found at index : "+i);
+            }
+        }
+
+        System.out.println(binSearch(Box,0,Box.length,67));
     }
 }
