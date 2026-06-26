8a3305a39ea44d61b7349afba40806615c0cc200
Dhiraj70C <dchaudhari1256@gmail.com>
2026-06-26 16:51:51 +0530

    added code

---
+++ Advance/Day11.java
@@ -0,0 +1,27 @@
+/*
+
+can a array have duplicate elements
+
+can an array store diff datatypes
+
+is an array size fixed after creation 
+
+can you access arr[arr.length];
+ 
+are arrays objects in java;
+
+
+*/
+
+class Day11{
+
+    public static void main(String args[]){
+
+        int a = 10;
+        int b = -20;
+
+        int c = a+b;
+        System.out.println(c);
+        System.out.println(a+b);
+    }
+}
