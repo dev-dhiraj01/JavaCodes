fda68448990d2619db74842d8481458ca0819202
Dhiraj70C <dchaudhari1256@gmail.com>
2026-07-05 20:25:24 +0530

    added filter

--- Advance/MultipleThreads.java
+++ Advance/MultipleThreads.java
@@ -1,11 +1,12 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.*;
 
 class MultipleThreads{
 
     public static void main(String args[]){
-        int size = 1_000;
+        int size = 1_0;
 
         List<Integer> nums = new ArrayList<>(size);
 
@@ -16,5 +17,11 @@ class MultipleThreads{
         }
 
         System.out.println(nums);
+
+        int sum = nums.stream()
+                        .map(i -> i*2)
+                        .reduce(0,(c,e) -> c+e);
+
+        System.out.println(sum);
     }
 }
