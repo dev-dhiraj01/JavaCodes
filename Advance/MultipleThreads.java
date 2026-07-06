e5e9b8b3e53b2f55bc6865433440d2e0623ef226
Dhiraj70C <dchaudhari1256@gmail.com>
2026-07-06 16:10:16 +0530

    added time :

--- Advance/MultipleThreads.java
+++ Advance/MultipleThreads.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.stream.*;
+import java.util.stream.Stream;
 
 class MultipleThreads{
 
@@ -18,10 +18,14 @@ class MultipleThreads{
 
         System.out.println(nums);
 
+        long currstr = System.currentTimeMillis();
         int sum = nums.stream()
                         .map(i -> i*2)
                         .reduce(0,(c,e) -> c+e);
 
+        long endstr = System.currentTimeMillis();
+
         System.out.println(sum);
+        System.out.println("The time required is : "+ ( endstr - currstr));
     }
 }
