c7f20c5cf2ef9ec8d7ecee323d2c215d050de15b
Dhiraj70C <dchaudhari1256@gmail.com>
2026-09-23 15:05:04 +0530

    added code for characters patterns

--- Advance/reg.java
+++ Advance/reg.java
@@ -1,3 +1,12 @@
+import java.util.regex.*;
+
 public class reg {
-    
+    public static void main(String[] args) {
+        Pattern p = Pattern.compile("[a-z]");
+        Matcher m = p.matcher("a7b@z#9");
+
+        while(m.find()){
+             System.out.println(m.start()+ ".." +m.group());
+        }
+    }
 }
