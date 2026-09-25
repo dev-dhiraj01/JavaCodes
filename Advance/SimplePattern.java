d03a09e3b1383f2bc0317b17fc8f03fa24e79364
Dhiraj70C <dchaudhari1256@gmail.com>
2026-09-25 22:12:34 +0530

    added code

--- Advance/SimplePattern.java
+++ Advance/SimplePattern.java
@@ -2,12 +2,22 @@ public class SimplePattern {
     public static void main(String args[]){
         int n = 5;
 
-        for(int row = 1; row <= n; row++){
-            for(int col = 1; col <= row; col++){
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
 
     }
 }
+
+
+        // int n = 5;
+
+        // for(int row = 1; row <= n; row++){
+        //     for(int col = 1; col <= row; col++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        //     * 
+        //     * * 
+        //     * * * 
+        //     * * * * 
+        //     * * * * * 
