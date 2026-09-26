b9380cf32ef4f3ea072f1774f0a5d34e7833322c
Dhiraj70C <dchaudhari1256@gmail.com>
2026-09-26 18:33:06 +0530

    added Alphabet code

--- Advance/SimplePattern.java
+++ Advance/SimplePattern.java
@@ -1,11 +1,69 @@
 public class SimplePattern {
     public static void main(String args[]) {
 
+        int n = 5; 
+
+        for(int row = 1; row <= n; row++){
+            char ch = 'A';
+            for(int col = 1; col <= row; col++){
+                System.out.print(ch+" ");
+                ch++;
+            }
+            System.out.println();
+        }
     }
 }
 
+        //         int n = 4;
+
+        // for (int row = 1; row <= n; row++) {
+        //     for (int space = 1; space <= n - row; space++) {
+        //         System.out.print("  ");
+        //     }
+
+        //     if (row == 1) {
+        //         for (int col = 1; col <= 2 * row - 1; col++) {
+        //             System.out.print("* ");
+        //         }
+        //     } else {
+        //         System.out.print("* ");
+
+        //         for (int col = 1; col <= 2 * row - 3; col++) {
+        //             System.out.print("  ");
+        //         }
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // for (int row = n-1; row >= 1; row--) {
+        //     for (int space = 1; space <= n - row; space++) {
+        //         System.out.print("  ");
+        //     }
+
+        //     if (row == 1) {
+        //         for (int col = 1; col <= 2 * row - 1; col++) {
+        //             System.out.print("* ");
+        //         }
+        //     } else {
+        //         System.out.print("* ");
+
+        //         for (int col = 1; col <= 2 * row - 3; col++) {
+        //             System.out.print("  ");
+        //         }
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+        //       * 
+        //     *   * 
+        //   *       * 
+        // *           * 
+        //   *       * 
+        //     *   * 
+        //       * 
         //      Dymand Patteren
-        
+
         //         int n = 5;
         
         // for(int row = 1; row <= n; row++){
